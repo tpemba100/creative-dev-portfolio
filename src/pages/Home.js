423f4f8c1dff6e4a997737115b169f934a7e23df
tpemba100 <tpemba.96@gmail.com>
2024-08-19 19:22:14 -0400

    new branch check

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,10 +2,8 @@ import About from ".././components/About/About";
 import Projects from ".././components/Projects/Projects";
 import Contact from ".././components/Contact/Contact";
 import ".././App.css";
-import ShowRepo from ".././components/Repo/ShowRepo";
 import Hero from ".././components/Hero/Hero";
 import { projects } from "../portfolio";
-import { Link } from "react-router-dom";
 
 const Home = () => {
   if (!projects.length) return null;
@@ -15,18 +13,7 @@ const Home = () => {
       <Hero />
       <About />
       <Projects projects={projects} />
-      <Link to="/archive" className="custom-link">
-        <div style={{ width: "170px", margin: "auto", marginTop: "1rem" }}>
-          <span
-            type="button"
-            className="btn btn--outline"
-            style={{ textAlign: "center" }}
-          >
-            more in Archive
-          </span>
-        </div>
-      </Link>
-      <ShowRepo />
+
       <Contact />
     </div>
   );
